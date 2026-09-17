1c290a63679d2b23c18e1e9d169a099c16e871c1
arunbew32-tech <arunbew32@gmail.com>
2026-09-17 14:23:09 +0100

    Added Black scholes as a function

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import numpy as np
-import math
 from scipy.stats import norm
 # values
 
-S_0 = 1.2 # stock price
-r = 0.02 # risk free rate
-T = 0.5 # time to maturity (y)
-X = 1.25 # strike price
-v = 0.25 # volatility
 
-d_1 = ((np.log(S_0/X)) + (r + (v**(2))/2)*T)/(v*(np.sqrt(T)))
+#defining a function to run the whole operation in one go
+def Black_Scholes_call(S_0, r,T,X,sigma):
+    d_1 = ((np.log(S_0/X)) + (r + (sigma**(2))/2)*T)/(sigma*(np.sqrt(T)))
+    d_2 = d_1 - sigma*np.sqrt(T)
+    
+    C = S_0*norm.cdf(d_1) - X * np.exp(-r * T) * norm.cdf(d_2)
 
-d_2 = d_1 - v*np.sqrt(T)
+    return C
 
-C = S_0*norm.cdf(d_1) - X * np.exp(-r * T) * norm.cdf(d_2)
+call_price = Black_Scholes_call(1.2, 0.02, 0.5, 1.25, 0.25)
 
-print(f"The price of your call option is ${C:.3f}")
+print(f"The price of your call option is ${call_price:.3f}")
