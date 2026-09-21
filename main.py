fdc9675016dcc131e5f6dbc850cfe01a169be6d9
arunbew32-tech <arunbew32@gmail.com>
2026-09-21 15:59:09 +0100

    introduced very simple put-call parity check

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import numpy as np
 from scipy.stats import norm
-# values
+# assumptions the model is making:
+# volatility is constant 
+# risk free rate is constant 
+# no divideds during the options time frame
+# all motions are relatively within normal processes and no large jumps
+# No transaction costs or taxes
 
-
-#defining a function to run the whole operation in one go
 def black_scholes(S_0, r,T,X,sigma, option_type):
     d_1 = ((np.log(S_0/X)) + (r + (sigma**(2))/2)*T)/(sigma*(np.sqrt(T)))
     d_2 = d_1 - sigma*np.sqrt(T)
+    
 
     if option_type == "call":
         C = S_0*norm.cdf(d_1) - X * np.exp(-r * T) * norm.cdf(d_2)
@@ -16,15 +20,13 @@ def black_scholes(S_0, r,T,X,sigma, option_type):
         return P
     else:
         return("Error")
+    
+def put_call_parity(S_0, r,T,X):
+    return S_0 - X*np.exp(-r*T)
 
-price = black_scholes(1.2, 0.02, 0.5, 1.25, 0.25, "call")
+call_price = black_scholes(1.2, 0.02, 0.5, 1.25, 0.25, "call")
+put_price = black_scholes(1.2,0.02,0.5,1.25,0.25,"put")
 
-print(f"Your option price is {price}")
+discrepancy = (call_price - put_price) - put_call_parity(1.2, 0.02, 0.5, 1.25)
 
-
-# assumptions the model is making:
-# volatility is constant 
-# risk free rate is constant 
-# no divideds during the options time frame
-# all motions are relatively within stochatic processes and no large jumps
-# No transaction costs or taxes
+print(f"Discrepancy:  {discrepancy:.5f}  ")
